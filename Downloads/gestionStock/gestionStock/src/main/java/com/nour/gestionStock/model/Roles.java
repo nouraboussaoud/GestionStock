8c9c2456ee396cc4e70b687c800c3fe4aa3e641c
nouraboussaoud <nour.aboussaoud@esprit.tn>
2024-08-19 13:40:20 +0100

    entities updatesss

--- Downloads/gestionStock/gestionStock/src/main/java/com/nour/gestionStock/model/Roles.java
+++ Downloads/gestionStock/gestionStock/src/main/java/com/nour/gestionStock/model/Roles.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
